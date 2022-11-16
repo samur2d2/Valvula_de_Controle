5667277d1f3f357287718dfbe53105added22d3d
samur2d2 <samuribeiro@ucl.br>
2022-11-16 14:41:34 -0300

    para calibrar o PID

---
+++ espPID/stepMotor.ino
@@ -0,0 +1,35 @@
+void rst_A4988(){
+  digitalWrite(RST, LOW);
+  digitalWrite(RST, HIGH);
+}
+//=========================================================================================
+void disa_A4988(){digitalWrite(ENA, HIGH);}
+void ena_A4988(){digitalWrite(ENA, LOW);}
+void HOR(){digitalWrite(DIR, HIGH);}
+void AHR(){digitalWrite(DIR, LOW);}
+//=========================================================================================
+void PASSO(){
+  digitalWrite(STP, LOW);
+  delayMicroseconds (MeioPeriodo);
+  digitalWrite(STP, HIGH);
+  delayMicroseconds (MeioPeriodo);
+}
+//=========================================================================================
+void FULL(){
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+  digitalWrite(MS3, LOW);
+}
+//=========================================================================================
+void P1_8(){
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, LOW);
+}
+//=========================================================================================
+void TesteMotor(int nPassos){
+  ena_A4988();
+  P1_8();
+  for (int i=0; i<=nPassos; i++){PASSO();}
+  disa_A4988();
+}
