9e7201da3a7f2433d6afd39e2cdf3dd40941fed7
samur2d2 <samuribeiro@ucl.br>
2022-11-24 12:02:57 -0300

    Adicionada função para conectar com um IPV4 especifico

--- PID_ESP_STEP/ESPfiles/wifiConnection.ino
+++ PID_ESP_STEP/ESPfiles/wifiConnection.ino
@@ -1,9 +1,16 @@
 //=========================================================================================
+IPAddress ip(192, 168, 0, 10);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
 void setup_wifi() {
   delay(10);
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.print(ssid);
+  Serial.print(" as ");
+  Serial.println(ip);
+  WiFi.config(ip, gateway, subnet);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
